77a073047cb31f76f5cd002ea94df68b2279046f
unknown <ijlnflhbrzdev@gmail.com>
2022-06-29 16:18:29 +0700

    +[UPDATE] Scrollable horizontal in web

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:movie_toz/introduction_screen.dart';
 import 'package:get/get.dart';
+import 'package:flutter/gestures.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,10 +14,34 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const GetMaterialApp(
+    return GetMaterialApp(
+      scrollBehavior: MyCustomScrollBehavior(),
       debugShowCheckedModeBanner: false,
       title: 'Movietoz',
       home: IntroductionScreenMovies(),
+      builder: (context, child) => ResponsiveWrapper.builder(
+        child,
+        maxWidth: 1200,
+        minWidth: 480,
+        defaultScale: true,
+        breakpoints: [
+          ResponsiveBreakpoint.resize(480, name: MOBILE),
+          ResponsiveBreakpoint.autoScale(800, name: TABLET),
+          ResponsiveBreakpoint.resize(1000, name: DESKTOP),
+        ],
+        background: Container(
+          color: Color(0xFFF5F5F5),
+        ),
+      ),
     );
   }
+}
+
+class MyCustomScrollBehavior extends MaterialScrollBehavior {
+  // Override behavior methods and getters like dragDevices
+  @override
+  Set<PointerDeviceKind> get dragDevices => {
+        PointerDeviceKind.touch,
+        PointerDeviceKind.mouse,
+      };
 }
--- lib/pages/welcome.dart
+++ lib/pages/welcome.dart
@@ -12,47 +12,43 @@ class Welcome extends StatelessWidget {
       backgroundColor: cPrimaryColor,
       body: SafeArea(
         child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Image.asset('assets/splash-screen-logo.png'),
-              Text('WELCOME TO MOVIETOZ!',
-                  style: cWhiteTextStyle.copyWith(fontSize: 30)),
-              Text('Click The Button To Get Started',
-                  style: cGreyTextStyle.copyWith(fontSize: 18)),
-              const SizedBox(height: 120),
-              Container(
-                width: 330,
-                height: 47,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20.0),
-                  color: Color(0xff373f85),
-                  // gradient: const LinearGradient(
-                  //   colors: [
-                  //     // Color.fromARGB(255, 88, 88, 88),
-                  //     Color.fromARGB(169, 136, 91, 1),
-                  //     Color(0XFFFF722A)
-                  //   ],
-                  // ),
-                ),
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      primary: Colors.transparent,
-                      shadowColor: Colors.transparent),
-                  onPressed: () {
-                    // Navigator.pushReplacement(
-                    //     context,
-                    //     MaterialPageRoute(
-                    //         builder: (BuildContext context) => const Login()));
-                    Get.to(Login());
-                  },
-                  child: Text(
-                    'GET STARTED',
-                    style: cWhiteTextStyle.copyWith(fontSize: 18),
+          child: Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Expanded(child: Image.asset('assets/splash-screen-logo.png')),
+                Text('WELCOME TO MOVIETOZ!',
+                    style: cWhiteTextStyle.copyWith(fontSize: 30)),
+                Text('Click The Button To Get Started',
+                    style: cGreyTextStyle.copyWith(fontSize: 18)),
+                const SizedBox(height: 50),
+                Container(
+                  width: 330,
+                  height: 47,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20.0),
+                    color: Color(0xff373f85),
+                  ),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        primary: Colors.transparent,
+                        shadowColor: Colors.transparent),
+                    onPressed: () {
+                      // Navigator.pushReplacement(
+                      //     context,
+                      //     MaterialPageRoute(
+                      //         builder: (BuildContext context) => const Login()));
+                      Get.to(Login());
+                    },
+                    child: Text(
+                      'GET STARTED',
+                      style: cWhiteTextStyle.copyWith(fontSize: 18),
+                    ),
                   ),
                 ),
-              ),
-            ],
+                const SizedBox(height: 50),
+              ],
+            ),
           ),
         ),
       ),
